ac4e5ff7406488033e330ccfb8084276be1adae3
Enrique <enriquec@mit.edu>
2023-01-26 22:52:43 -0500

    Created global store

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+  import { SettingsStore } from '../Store';
+</script>
+
 <h1 class="bg-red-200">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-xl">{$SettingsStore.name}</p>
