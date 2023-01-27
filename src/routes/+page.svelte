909e05b4fa78f2ac1e639a662e70635691abd535
Enrique <enriquec@mit.edu>
2023-01-27 12:35:40 -0500

    Testing interactive state

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script>
   import { SettingsStore } from '../Store';
+
+  const updateState = () => {
+    SettingsStore.update(() => ({
+      name: 'wow'
+    }));
+  };
 </script>
 
 <h1 class="bg-red-200">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <p class="text-xl">{$SettingsStore.name}</p>
+<button on:click={updateState}>Click me</button>
